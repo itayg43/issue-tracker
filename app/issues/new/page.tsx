f23825ae1339ee17c7df2617deede3f45ca3d9a6
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-05-15 19:42:26 +0300

    edit: handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 
@@ -22,31 +23,43 @@ const NewIssuePage = () => {
     formState: { errors, isSubmitting },
   } = useForm<IssueFormData>();
 
+  const [isSubmittingFailed, setIsSubmittingFailed] = useState(false);
+
   const onSubmit = handleSubmit(async (data) => {
     try {
       await axios.post("/api/issues", data);
       router.push("/issues");
-    } catch (error) {}
+    } catch (error) {
+      setIsSubmittingFailed(true);
+    }
   });
 
   return (
-    <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
+    <div className="max-w-xl">
+      {isSubmittingFailed && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>An unexpected error occurred.</Callout.Text>
+        </Callout.Root>
+      )}
 
-      <Controller
-        control={control}
-        name="description"
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+      <form className="space-y-3" onSubmit={onSubmit}>
+        <TextField.Root>
+          <TextField.Input placeholder="Title" {...register("title")} />
+        </TextField.Root>
 
-      <Button type="submit" disabled={isSubmitting}>
-        Submit New Issue
-      </Button>
-    </form>
+        <Controller
+          control={control}
+          name="description"
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <Button type="submit" disabled={isSubmitting}>
+          Submit New Issue
+        </Button>
+      </form>
+    </div>
   );
 };
 
