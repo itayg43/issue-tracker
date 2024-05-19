815ec0bdae9ecc803210b957243bb7f8e776c7dc
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-05-19 19:16:32 +0300

    edit: build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Pagination from "./issues/list/Pagination";
+
 const Home = () => {
-  return <div>Dashboard Page</div>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={1} />;
 };
 
 export default Home;
