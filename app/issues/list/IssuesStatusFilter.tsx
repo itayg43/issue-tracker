5582d0dacc48f97abf6f09d59db05fed38e62fff
itayg43 <93944494+itayg43@users.noreply.github.com>
2024-05-19 18:30:57 +0300

    edit: make columns sortable

--- app/issues/list/IssuesStatusFilter.tsx
+++ app/issues/list/IssuesStatusFilter.tsx
@@ -8,10 +8,21 @@ const statuses: {
   label: string;
   value?: Status;
 }[] = [
-  { label: "All" },
-  { label: "Open", value: "OPEN" },
-  { label: "In Progress", value: "IN_PROGRESS" },
-  { label: "Closed", value: "CLOSED" },
+  {
+    label: "All",
+  },
+  {
+    label: "Open",
+    value: "OPEN",
+  },
+  {
+    label: "In Progress",
+    value: "IN_PROGRESS",
+  },
+  {
+    label: "Closed",
+    value: "CLOSED",
+  },
 ];
 
 const IssuesStatusFilter = () => {
